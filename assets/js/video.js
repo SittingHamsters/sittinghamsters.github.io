c086aec864ebb6db40aacd63fbd585a943797952
Jakub Macina <jakub.macina@gmail.com>
2015-05-08 21:59:55 +0200

    Add video on image click.

---
+++ assets/js/video.js
@@ -0,0 +1,4 @@
+$('#screen').on('click',function(e){
+    $('#screen').hide();
+	$('#video').html('<iframe width="708" height="400" src="https://www.youtube.com/embed/XQ_NEuqWQ-s?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+});
